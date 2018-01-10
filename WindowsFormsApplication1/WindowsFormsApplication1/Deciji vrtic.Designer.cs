ed71d28d9ff38244a0a79aa198ea93f8421d7498
TheHeadhog <stevan.popovic.headhog@gmail.com>
2018-01-11 00:54:35 +0100

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Deciji vrtic.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnPotvrdi = new System.Windows.Forms.Button();
+            this.btnAzuriraj = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbGrupa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbOpstina = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPrezIme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnOtvori = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.prezIme = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datrodj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.opstina = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grupa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(7, 7);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(783, 457);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnOdustani);
+            this.tabPage1.Controls.Add(this.btnPotvrdi);
+            this.tabPage1.Controls.Add(this.btnAzuriraj);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.tbGrupa);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.tbOpstina);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.tbPrezIme);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(775, 428);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Upis";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Enabled = false;
+            this.btnOdustani.Location = new System.Drawing.Point(323, 361);
+            this.btnOdustani.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(100, 28);
+            this.btnOdustani.TabIndex = 12;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // btnPotvrdi
+            // 
+            this.btnPotvrdi.Enabled = false;
+            this.btnPotvrdi.Location = new System.Drawing.Point(179, 361);
+            this.btnPotvrdi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPotvrdi.Name = "btnPotvrdi";
+            this.btnPotvrdi.Size = new System.Drawing.Size(100, 28);
+            this.btnPotvrdi.TabIndex = 11;
+            this.btnPotvrdi.Text = "Potvrdi";
+            this.btnPotvrdi.UseVisualStyleBackColor = true;
+            this.btnPotvrdi.Click += new System.EventHandler(this.btnPotvrdi_Click);
+            // 
+            // btnAzuriraj
+            // 
+            this.btnAzuriraj.Location = new System.Drawing.Point(35, 361);
+            this.btnAzuriraj.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAzuriraj.Name = "btnAzuriraj";
+            this.btnAzuriraj.Size = new System.Drawing.Size(100, 28);
+            this.btnAzuriraj.TabIndex = 10;
+            this.btnAzuriraj.Text = "Azuriraj";
+            this.btnAzuriraj.UseVisualStyleBackColor = true;
+            this.btnAzuriraj.Click += new System.EventHandler(this.btnAzuriraj_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(421, 293);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(135, 24);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Text = "Spisak opstina";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(35, 293);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "Spisak dece";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tbGrupa
+            // 
+            this.tbGrupa.Enabled = false;
+            this.tbGrupa.Location = new System.Drawing.Point(437, 185);
+            this.tbGrupa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbGrupa.Name = "tbGrupa";
+            this.tbGrupa.Size = new System.Drawing.Size(103, 22);
+            this.tbGrupa.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(373, 188);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Grupa>";
+            // 
+            // tbOpstina
+            // 
+            this.tbOpstina.Location = new System.Drawing.Point(157, 185);
+            this.tbOpstina.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOpstina.Name = "tbOpstina";
+            this.tbOpstina.Size = new System.Drawing.Size(169, 22);
+            this.tbOpstina.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Opstina rodjenja>";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(156, 101);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Datum rodjenja>";
+            // 
+            // tbPrezIme
+            // 
+            this.tbPrezIme.Location = new System.Drawing.Point(156, 46);
+            this.tbPrezIme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPrezIme.Name = "tbPrezIme";
+            this.tbPrezIme.Size = new System.Drawing.Size(169, 22);
+            this.tbPrezIme.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prezime i ime>";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnOtvori);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(775, 428);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Pregled";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnOtvori
+            // 
+            this.btnOtvori.Location = new System.Drawing.Point(345, 370);
+            this.btnOtvori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOtvori.Name = "btnOtvori";
+            this.btnOtvori.Size = new System.Drawing.Size(117, 34);
+            this.btnOtvori.TabIndex = 1;
+            this.btnOtvori.Text = "Otvori";
+            this.btnOtvori.UseVisualStyleBackColor = true;
+            this.btnOtvori.Click += new System.EventHandler(this.btnOtvori_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.prezIme,
+            this.datrodj,
+            this.opstina,
+            this.grupa});
+            this.dataGridView1.Location = new System.Drawing.Point(8, 7);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 341);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // prezIme
+            // 
+            this.prezIme.HeaderText = "Prezime i ime";
+            this.prezIme.Name = "prezIme";
+            this.prezIme.Width = 200;
+            // 
+            // datrodj
+            // 
+            this.datrodj.HeaderText = "Dat.rodjenja";
+            this.datrodj.Name = "datrodj";
+            this.datrodj.Width = 130;
+            // 
+            // opstina
+            // 
+            this.opstina.HeaderText = "Opstina";
+            this.opstina.Name = "opstina";
+            // 
+            // grupa
+            // 
+            this.grupa.HeaderText = "Grupa";
+            this.grupa.Name = "grupa";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chart1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Size = new System.Drawing.Size(775, 428);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Prikaz";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(8, 7);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(753, 410);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 475);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Deciji vrtic";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPrezIme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox tbGrupa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbOpstina;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnPotvrdi;
+        private System.Windows.Forms.Button btnAzuriraj;
+        private System.Windows.Forms.Button btnOtvori;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prezIme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datrodj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn opstina;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grupa;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
